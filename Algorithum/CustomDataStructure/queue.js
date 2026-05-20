0949e937e1f27fd0fa6fd1a4bbff3090f9efc804
osama_khan18 <badishakhan18@gmail.com>
2026-05-20 12:51:01 +0500

    add the work

---
+++ Algorithum/CustomDataStructure/queue.js
@@ -0,0 +1,36 @@
+class Queue{
+    constructor(){
+        this.items =[]
+    }
+enqueue(...elements){
+    this.items.push(...elements)
+
+}
+dequeue(){
+    this.items.shift()
+}
+isEmpty(){
+    return this.items.length==0
+}
+peek(){
+    if(!this.isEmpty()){
+        return this.items[0]
+    }
+    return null
+}
+size(){
+   return  this.items.length
+}
+}
+
+const q1 = new Queue
+q1.enqueue(10,20)
+console.log(q1.items)
+q1.dequeue()
+console.log(q1.items)
+q1.isEmpty()
+
+console.log(q1.items)
+console.log(q1.size())
+
+
